04010e560332e968ea2b6e82e6ecdf9ece48aeb3
tavkeer <shahtavkeer@gmail.com>
2023-10-28 18:46:15 +0530

    update

--- jobhub_backend/middleware/verifyToken.js
+++ jobhub_backend/middleware/verifyToken.js
@@ -1,4 +1,35 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
 
-module.exports = {};
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verifyToken(token, process.env.JWT_SEC, async (err, user) => {
+      if (err) {
+        res.status(403).json("INVALID TOKEN");
+
+        req.user = user;
+
+        console.log(user);
+
+        next();
+      }
+    });
+  } else {
+    res.status(401).json("Your are not Authenticated");
+  }
+};
+
+const verifyAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.parms.id) {
+      next();
+    } else {
+      res.status(403).json("You are restricted to perform this operation");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyAndAuthorization };
